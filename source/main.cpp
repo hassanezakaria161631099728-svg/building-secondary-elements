e1c7bb8034cbb7106cd5539ad2abd4db707e62a2
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-09 23:08:23 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,17 @@
 #include "expressions.h"
 #include "io.h"
-
+#include <iostream>
 int main()
 {
-    double height_floor = 3.06;
-    int height_stairs = height_floor / 2 * 1000;
-    int vertical_step = 180;
-    int horizontal_step = 250;
+    // 1. Read TXT
+    BuildingData d = readInput("input.txt");
 
-    stair_result result = stairs(height_stairs, vertical_step, horizontal_step);
+    stair_result stair = stairs(d, d.height_floor, d.vertical_step, d.horizontal_step);
 
-    write_result("result.txt", result);
+ RC_column_result  column = RC_column(d ,d.S, d.G1 , d.G2 , d.n_floors , d.fc28 , d.fe,
+                            d.Q_rooftop ,d.Q_current_floor ,d.Q_ground_floor);
+
+    write_result("result.txt", stair, column);
 
     return 0;
 }
