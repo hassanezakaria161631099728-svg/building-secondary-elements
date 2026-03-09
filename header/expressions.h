75814b9b00d1ae456ba48c1fb0a58acae66fc47f
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-09 23:08:59 +0100

    Add files via upload

--- header/expressions.h
+++ header/expressions.h
@@ -1,12 +1,37 @@
 #pragma once
+
+struct BuildingData {
+    // stairs
+    double height_floor;
+    int vertical_step,horizontal_step;// stiffness Mass Dynamic matrices
+    // column
+    double S;
+    int G1, G2, n_floors, fc28, fe, Q_rooftop, Q_current_floor, Q_ground_floor;
+
+};
+
 struct stair_result
 {
     int height_stairs;
-    int vertical_step;
     int n_stairs;
-    int horizontal_step;
     int length_stairs;
     bool rule_valid;
 };
 
-stair_result stairs(int height_stairs, int vertical_step,int horizontal_step);
+stair_result stairs(const BuildingData& d, double height_floor, int vertical_step,int horizontal_step);
+
+struct RC_column_result
+{
+    double Smaj;
+    double NG;
+    double NQ;
+    double Nu;
+    double alpha;
+    double gamma_c;
+    double gamma_s;
+    double Br_min;
+    double a_min;
+};
+
+RC_column_result  RC_column(const BuildingData& d, double S, int G1 ,int G2 ,int n_floors ,
+                            int fc28 ,int fe,int Q_rooftop ,int Q_current_floor ,int Q_gound_floor);
--- header/io.h
+++ header/io.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "expressions.h"
+BuildingData readInput(const char* filename);
 
-void write_result(const char* filename, const stair_result& result);
+void write_result(const char* filename, const stair_result& stair,
+                  const RC_column_result& column);
 
